38453a5538b7e943284403c68501db11d25e5f5d
Romm1002 <romain.chaumont@hotmail.fr>
2023-05-23 17:03:56 +0200

    prettier

--- src/components/typePill.js
+++ src/components/typePill.js
@@ -1,10 +1,16 @@
-const TypePill = ({type, color}) => {
-    if(color == undefined){
-        color = "gray"
-    }
-    return ( 
-        <li key={type} className="badge badge-pill m-1" style={{backgroundColor: color}}>{type}</li>
-     );
-}
- 
+const TypePill = ({ type, color }) => {
+  if (color == undefined) {
+    color = "gray";
+  }
+  return (
+    <li
+      key={type}
+      className="badge badge-pill m-1"
+      style={{ backgroundColor: color }}
+    >
+      {type}
+    </li>
+  );
+};
+
 export default TypePill;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,37 @@
 import React from "react";
 import "../assets/styles/style.css";
 import { Link } from "react-router-dom";
-import typeColor from "../data/typeColors.json"
+import typeColor from "../data/typeColors.json";
 import TypePill from "../components/typePill";
 
 function Home({ pokemons }) {
   const elements = pokemons.map((pokemon) => {
     return (
-      <Link key={pokemon.id} to={`/details/${pokemon.id}`} className="card px-3 py-1 mb-5 me-3 col-lg-2 col-sm-3 bg-light">
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} className="card-img-top"></img>
-        <h2 className="h4">#{pokemon.id} - {pokemon.nom}</h2>
+      <Link
+        key={pokemon.id}
+        to={`/details/${pokemon.id}`}
+        className="card px-3 py-1 mb-5 me-3 col-lg-2 col-sm-3 bg-light"
+      >
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+          className="card-img-top"
+        ></img>
+        <h2 className="h4">
+          #{pokemon.id} - {pokemon.nom}
+        </h2>
         <ul>
-          {pokemon.types.map((type) => <TypePill key={type} type={type} color={typeColor[type]}/>)}
+          {pokemon.types.map((type) => (
+            <TypePill key={type} type={type} color={typeColor[type]} />
+          ))}
         </ul>
       </Link>
     );
   });
-  return <main className="container">
-    <div className="row justify-content-around">
-      {elements}
-    </div>
-  </main>;
+  return (
+    <main className="container">
+      <div className="row justify-content-around">{elements}</div>
+    </main>
+  );
 }
 
 export default Home;
